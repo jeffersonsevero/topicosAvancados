e89551be6ff5dc1a0a65142874e3986a0f208c58
jeffersonsevero <37408502+jeffersonsevero@users.noreply.github.com>
2019-06-22 15:56:10 -0300

    Update instalacaoWordpressSemBD.sh

--- instalacaoWordpressSemBD.sh
+++ instalacaoWordpressSemBD.sh
@@ -3,6 +3,8 @@
 #Esse script necessecita do IP do banco de dados
 #O mesmo deve ser exportado com no IPBD
 ip_banco="${IPBD}"
+usuario="wordpress"
+senha="wordpress"
 
 #INSTALA O O QUE É NECESSÁRIO
 sudo apt -y update
@@ -56,3 +58,29 @@ sudo sed -i "s/database_name_here/wordpress/g" /var/www/html/wordpress/wp-config
 sudo sed -i "s/username_here/wordpress/g" /var/www/html/wordpress/wp-config.php 
 sudo sed -i "s/password_here/wordpress/g" /var/www/html/wordpress/wp-config.php
 sudo sed -i "s/localhost/$ip_banco/g" /var/www/html/wordpress/wp-config.php
+
+
+sed -i "s/\$language = '';/\$language = 'pt_BR';/" /var/www/html/wordpress/wp-admin/install.php
+
+n=`grep -n '$step =' $arquivo | cut -f 1 -d :`
+sed -i ""$n"s/: 0;/: 2;/" /var/www/html/wordpress/wp-admin/install.php
+
+aux=`grep -n '$weblog_title[[:blank:]]*=' /var/www/html/wordpress/wp-admin/install.php | cut -f 1 -d ":"`
+n=`echo $aux | cut -f 2 -d " "`
+sed -i ""$n"s/: '';/: 'TituloSite';/" /var/www/html/wordpress/wp-admin/install.php
+
+aux=`grep -n '$user_name[[:blank:]]*=' /var/www/html/wordpress/wp-admin/install.php | cut -f 1 -d ":"`
+n=`echo $aux | cut -f 2 -d " "`
+sed -i ""$n"s/: '';/: '"$usuario"';/" /var/www/html/wordpress/wp-admin/install.php
+
+n=`grep -n '$admin_password[[:blank:]]*=' /var/www/html/wordpress/wp-admin/install.php | cut -f 1 -d :`
+sed -i ""$n"s/: '';/: '"$senha"';/" /var/www/html/wordpress/wp-admin/install.php
+
+n=`grep -n '$admin_password_check[[:blank:]]*=' /var/www/html/wordpress/wp-admin/install.php | cut -f 1 -d :`
+sed -i ""$n"s/: '';/: '"$senha"';/" /var/www/html/wordpress/wp-admin/install.php
+
+aux=`grep -n '$admin_email[[:blank:]]*=' /var/www/html/wordpress/wp-admin/install.php | cut -f 1 -d ":"`
+n=`echo $aux | cut -f 2 -d " "`
+sed -i ""$n"s/: '';/: 'admin@email.com';/" /var/www/html/wordpress/wp-admin/install.php
+
+
